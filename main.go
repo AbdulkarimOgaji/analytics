367f92ee81f39ba5a45b5283fca5b5956bef7c4f
AbdulkarimOgaji <abdulkarimogaji002@gmail.com>
2022-08-03 14:06:16 +0100

    feat: add logging of data

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"time"
 )
 
 
@@ -24,6 +25,19 @@ type IPResponse struct {
 	IP 					net.IP `json:"ip"`
 }
 
+type ActionParams struct {
+	Type 				string `json:"type"`
+	Source 			string `json:"source"`
+	Description string `json:"description"`
+}
+
+var knownIps = map[string]string{
+  "154.113.68.102": "Abdulkarim's Laptop",
+  "102.89.34.128" : "Abdulkarim's Phone",
+	"197.210.226.113": "Mariams's Phone",
+	"102.89.33.246": "Unknown",
+	"102.89.32.245": "Ahmed's Phone",
+}
 
 
 
@@ -47,7 +61,18 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 
 
 func handleAnalytics(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "This endpoint expects a post request", http.StatusBadRequest)
+		return
+	}
 	// w.Header().Add("Content-Type", "application/json")
+	decoder := json.NewDecoder(r.Body)
+	var action ActionParams
+	err := decoder.Decode(&action)
+	if err != nil {
+		http.Error(w, "Failed to decode body" + err.Error(), http.StatusBadRequest)
+		return
+	}
 	ip := getIP(r)
 	log.Println(ip)
 	req, err := http.NewRequest("GET", "http://ip-api.com/json/" + ip.String() , nil)
@@ -63,14 +88,14 @@ func handleAnalytics(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	decoder := json.NewDecoder(resp.Body)
+	decoder = json.NewDecoder(resp.Body)
 
 	var jsonResp IPResponse
 	err = decoder.Decode(&jsonResp)
 	jsonResp.IP = ip
 
 
-	storeAnalytics(jsonResp)
+	storeAnalytics(jsonResp, action)
 	if err != nil {
 		fmt.Fprintf(w, "Failed to decode response" + err.Error())
 		return
@@ -108,6 +133,20 @@ func getIP(r *http.Request) net.IP {
 	}
 }
 
-func storeAnalytics(d IPResponse) {
-	// store or send email
+func storeAnalytics(d IPResponse, action ActionParams) {
+	reqTime := time.Now()
+	// check if ip is among known ips
+	for ip, own := range knownIps {
+		if d.IP.String() == ip {
+			log.Printf("%q just %q, description:\n %s\n on %q at %q time", own, action.Type, action.Description, action.Source, reqTime)
+			return
+		}
+	}
+
+	b, err := json.MarshalIndent(d, "", "	")
+	if err != nil {
+		log.Println("Failed to marshal ", err)
+	}else {
+		log.Println(string(b))
+	}
 }
